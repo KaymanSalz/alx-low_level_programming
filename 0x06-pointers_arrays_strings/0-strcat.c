45758b7de769b5c5c87f76c69a1d20bbcd2a1df4
Kayman <35963738+KaymanSalz@users.noreply.github.com>
2022-06-15 16:02:58 +0100

    new strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,13 +1,15 @@
 #include "main.h"
-/*
- *  _strcat - concatenate two strings
+
+/**
+ * _strcat - concatenate two strings
  * @dest: the string to append to
  * @src: the string to append
+ *
  * Description: This function appends the string pointed to by src to the
  * string pointed to by dest, overwriting the null byte at the end of dest.
+ *
  * Return: a pointer to the destination string dest
  */
-
 char *_strcat(char *dest, char *src)
 {
 	char *dest_end = dest;
@@ -28,4 +30,3 @@ char *_strcat(char *dest, char *src)
 
 	return (dest);
 }
-
